ff3d9b40de8fcdbf0ccc6a052c5643b088428325
jesus <jesagulop@gmail.com>
2026-09-12 09:55:56 +0200

    documented package goadd
    Added godoc comments to both function and package.

--- goadd/add.go
+++ goadd/add.go
@@ -1,5 +1,8 @@
 package goadd
 
+// Add takes two integer numbers and add thems together. It taks advantage
+// of the possibility of endowing Z with a group structure. Addition is a
+// commutative binary operation on the set of integers.
 func Add(a, b int) int {
 	return a + b
 }
---
+++ goadd/doc.go
@@ -0,0 +1,5 @@
+// Package goadd provides funcionality for the simples mathematical operation
+// integers: addition. Check [MathIsFun site] for details on how addition works.
+//
+// [MathIsFun site]: https://www.mathisfun.com/numbers/addition.html
+package goadd
